996181e3792ca6d77dd8c9c7d12fc927ea1584b3
akansha-maheshwari-axway <55788119+akansha-maheshwari-axway@users.noreply.github.com>
2020-07-28 17:51:50 +0530

    [MOD-2676, MOD-2677] feat(android): Connecting Devices
    expose socket class methods, maintain socket state and fireEvents for the same

--- android/src/appcelerator/bluetooth/BluetoothSocketProxy.java
+++ android/src/appcelerator/bluetooth/BluetoothSocketProxy.java
@@ -7,6 +7,10 @@ package appcelerator.bluetooth;
 
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+import java.io.IOException;
+import java.util.UUID;
+import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 
@@ -20,6 +24,8 @@ public class BluetoothSocketProxy extends KrollProxy
 	private BluetoothDevice bluetoothDevice;
 	private Thread connectThread;
 
+	private socketState state = socketState.open;
+
 	public BluetoothSocketProxy(BluetoothSocket bluetoothSocket, String uuid, boolean secure,
 								BluetoothDevice bluetoothDevice)
 	{
@@ -29,4 +35,95 @@ public class BluetoothSocketProxy extends KrollProxy
 		isSecure = secure;
 		this.bluetoothDevice = bluetoothDevice;
 	}
+
+	@Kroll.method
+	public void connect()
+	{
+		KrollDict dict = new KrollDict();
+		if (state != socketState.open) {
+			Log.e(LCAT, "Cannot connect socket, Socket: " + state);
+			return;
+		}
+		state = socketState.connecting;
+		connectThread = new Thread(() -> {
+			try {
+				btSocket.connect();
+				state = socketState.connected;
+				dict.put("socket", this);
+				fireEvent("connected", dict);
+			} catch (IOException connectException) {
+				state = socketState.error;
+				dict.put("socket", this);
+				dict.put("errorMessage", connectException.getMessage());
+				fireEvent("error", dict);
+			}
+		});
+		connectThread.start();
+	}
+
+	@Kroll.method
+	public boolean isConnected()
+	{
+		if (btSocket != null) {
+			return btSocket.isConnected();
+		}
+		return false;
+	}
+
+	@Kroll.method
+	public boolean isConnecting()
+	{
+		return state == socketState.connecting;
+	}
+
+	@Kroll.method
+	public void cancelConnect()
+	{
+		if (isConnecting()) {
+			try {
+				btSocket.close();
+				state = socketState.disconnected;
+			} catch (IOException e) {
+				Log.e(LCAT, "trying to cancel" + e.getMessage());
+			}
+			try {
+				if (isSecure) {
+					btSocket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString(uuid));
+					state = socketState.open;
+				} else {
+					btSocket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(UUID.fromString(uuid));
+					state = socketState.open;
+				}
+
+			} catch (IOException e) {
+				KrollDict dict = new KrollDict();
+				state = socketState.error;
+				dict.put("socket", this);
+				dict.put("errorMessage", "cannot create socket, Exception: " + e.getMessage());
+				fireEvent("error", dict);
+			}
+		} else {
+			Log.e(LCAT, "cannot cancel connection, Socket: " + state);
+		}
+	}
+
+	@Kroll.method
+	public void close()
+	{
+		KrollDict dict = new KrollDict();
+		try {
+			btSocket.close();
+			state = socketState.disconnected;
+			dict.put("socket", this);
+			dict.put("message", "Socket is Disconnected");
+			fireEvent("disconnected", dict);
+		} catch (IOException e) {
+			dict.put("socket", this);
+			dict.put("errorMessage", " trying to close socket but exception: " + e.getMessage());
+			fireEvent("error", dict);
+			Log.e(LCAT, e.getMessage());
+		}
+	}
+
+	private enum socketState { connecting, connected, disconnected, open, error }
 }
