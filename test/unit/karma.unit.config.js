cddb8f365c6e021e4babc73f030abffe69378a05
Joshua Quick <jquick@axway.com>
2021-08-23 15:44:55 -0700

    chore: build with SDK 10.1.0 and JDK 11

--- test/unit/karma.unit.config.js
+++ test/unit/karma.unit.config.js
@@ -13,7 +13,7 @@ module.exports = config => {
 			'karma-*'
 		],
 		titanium: {
-			sdkVersion: config.sdkVersion || '9.3.1.GA'
+			sdkVersion: config.sdkVersion || '10.1.0.v20210820083427'
 		},
 		customLaunchers: {
 			android: {
